8368856ab3f292fc61efebbff9d41dedbf54a422
Al Miller <almiller@Als-MacBook-Pro.local>
2023-03-22 17:19:37 -0400

    new leetocde files

--- leetcode/overloading.py
+++ leetcode/overloading.py
@@ -1,13 +1,10 @@
 
-
-
-# def myfunc(a,b, *args, **kwargs):
-
-#     print(a)
-#     print(args)
-#     print(kwargs)
-#     for ar in args:
-#         print(ar)
+def myfunc_1(a,b, *args, **kwargs):
+    print(a)
+    print(args)
+    print(kwargs)
+    for ar in args:
+        print(ar)
 
 def myfunc(a,b, **kwargs):
     print(a)
