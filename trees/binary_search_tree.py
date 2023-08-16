c42d5b89b8cf2a348e78b2057085f4d6aa00121b
albertcmiller1 <albertcmiller1@gmail.com>
2023-08-16 16:40:33 -0400

    trees

--- trees/binary_search_tree.py
+++ trees/binary_search_tree.py
@@ -1,3 +1,19 @@
+'''
+another way to insert: 
+TreeNode* insert(TreeNode* root, int val) {
+    if (!root) {
+        return new TreeNode(val);
+    }
+
+    if (val > root->val_) { 
+        root->right = insert(root->right, val);
+    } else if (val < root->val_) {
+        root->left = insert(root->left, val);
+    }
+    return root;
+}
+'''
+
 class Node: 
     def __init__(self, value):
         self.value = value 
@@ -13,6 +29,7 @@ class BinarySearchTree:
         if self.root == None: 
             self.root = new_node
             return True
+        
         temp = self.root
         while True: 
             # if value is already in the tree .. return 
@@ -38,7 +55,30 @@ tree = BinarySearchTree()
 tree.insert(2)
 tree.insert(1)
 tree.insert(3)
+tree.insert(4)
+'''
+        2
+       / \
+      1   3
+           \
+            4  
+'''
+# print(tree.root.value)
+# print(tree.root.left.value)
+# print(tree.root.right.value)
+# print(tree.root.right.right.value)
 
-print(tree.root.value)
-print(tree.root.left.value)
-print(tree.root.right.value)
+
+def value_in_tree(root, target):
+    if not root: return False
+
+    if target > root.value: 
+        return value_in_tree(root.right, target)
+    elif target < root.value: 
+        return value_in_tree(root.left, target)
+    else: 
+        return True
+
+assert value_in_tree(tree.root, 3) == True
+assert value_in_tree(tree.root, 8) == False
+
