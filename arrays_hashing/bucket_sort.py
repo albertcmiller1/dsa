b9faca99226cfe105969732b18de771321121671
albertcmiller1 <albertcmiller1@gmail.com>
2023-08-16 12:08:03 -0400

    bucket

--- arrays_hashing/bucket_sort.py
+++
@@ -1,7 +0,0 @@
-arr = [1, 1, 1, 2, 2, 100]
-
-m = {} # index, count 
-
-# another way 
-
-m = {} # i (count) , values 
--- sorting/bucketsort.py
+++ sorting/bucketsort.py
@@ -13,13 +13,25 @@ or could use an array
 [2, 1, 3]
 
 
+O(n) time 
+not stable 
 '''
 
 
-arr = [2, 1, 2, 0, 0, 2]
-counts = [0, 0, 0]
 
-for n in arr:
-    counts[n] += 1
+def bucket_sort(arr: list[int]) -> list[int]:
 
-print(counts)
+    counts = [0 for _ in range(max(arr)+1)]
+    for n in arr: 
+        counts[n] += 1
+
+    i = 0
+    for j in range(len(counts)):
+        for _ in range(counts[j]):
+            arr[i] = j
+            i += 1
+
+    return arr
+
+assert bucket_sort([2, 1, 2, 0, 0, 2]) == [0, 0, 1, 2, 2, 2]
+assert bucket_sort([2, 1, 2, 1, 1, 3, 4]) == [1, 1, 1, 2, 2, 3, 4]
