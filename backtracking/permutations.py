4af04fb77f4670f0808b76f7781d24f6127be194
albertcmiller1 <albertcmiller1@gmail.com>
2023-09-08 20:53:14 -0400

    backtracking

---
+++ backtracking/permutations.py
@@ -0,0 +1,21 @@
+from typing import List
+def permute(nums: List[int]) -> List[List[int]]:
+    res = []
+
+    if len(nums) == 1:
+        return [nums[:]]
+
+    for _ in range(len(nums)):
+        n = nums.pop(0)
+        perms = permute(nums)
+
+        for perm in perms:
+            perm.append(n)
+
+        res.extend(perms)
+        nums.append(n)
+
+    return res
+
+if __name__ == "__main__":
+    print(permute([1, 2, 3]))
