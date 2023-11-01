0a025625632281603ae541e2e13fb7f4a4830446
albertcmiller1 <albertcmiller1@gmail.com>
2023-11-01 00:54:31 -0400

    graphs

--- graphs/matrix_dfs.py
+++ graphs/matrix_dfs.py
@@ -10,28 +10,39 @@ graph = [
     [0,1,0,0]
 ]
 
+ROWS, COLS = len(graph), len(graph[0])
 
-def dfs(grid, r, c, visit): 
-    ROWS, COLS = len(grid), len(grid[0])
+def is_last_position(r, c): 
+    return r == ROWS - 1 and c == COLS - 1
 
-    if (min(r, c) < 0 or r == ROWS or c == COLS or (r, c) in visit or grid[r][c] == 1):
+def is_ob(r, c):
+    return min(r, c) < 0 or r >= ROWS or c >= COLS
+
+def visited(r, c, visit): 
+    return (r, c) in visit
+
+def invalid(r, c): 
+    return graph[r][c] == 1
+
+def dfs(r, c, visit): 
+    if (is_ob(r,c) or visited(r, c, visit) or invalid(r, c)):
         return 0
 
-    if r == ROWS - 1 and c == COLS - 1: 
+    if is_last_position(r, c): 
         return 1
 
     visit.add((r, c))
 
     count = 0
-    count += dfs(grid, r+1, c, visit)
-    count += dfs(grid, r-1, c, visit)
-    count += dfs(grid, r, c+1, visit)
-    count += dfs(grid, r, c-1, visit)
+    count += dfs(r+1, c, visit)
+    count += dfs(r-1, c, visit)
+    count += dfs(r, c+1, visit)
+    count += dfs(r, c-1, visit)
 
     visit.remove((r, c))
     return count 
 
-assert dfs(graph, 0, 0, set()) == 2
+assert dfs(0, 0, set()) == 2
 
 # O(4^(N*M)) time 
 # O(N*M) memory 
