5eedc60fbb51858c9401c948b4678d19c4b5ca54
albertcmiller1 <albertcmiller1@gmail.com>
2024-03-18 17:48:40 -0400

    tricks

---
+++ data_structures/hashing/get_max_key.py
@@ -0,0 +1,12 @@
+# https://stackoverflow.com/questions/18296755/python-max-function-using-key-and-lambda-expression
+
+students = {
+    'Theodore': 19,
+    'Roxanne': 22,
+    'Mathew': 21,
+    'Betty': 20
+}
+
+
+print(max(students, key=students.get))
+
---
+++ data_structures/trees/trie_2.py
@@ -0,0 +1,27 @@
+words = ["foo", "food"]
+
+
+trie_head = {}
+for word in words:
+    cur = trie_head
+    for letter in word:
+        cur = cur.setdefault(letter, {})
+    cur['#'] = word
+
+print(trie_head)
+'''
+The setdefault() method returns the value of the item with the specified key.
+If the key does not exist, insert the key, with the specified value, see example below
+{
+    'f': {
+        'o': {
+            'o': {
+                '#': 'foo', 
+                'd': {
+                    '#': 'food'
+                }
+            }
+        }
+    }
+}
+'''
